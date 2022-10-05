dd41725adbb4571a1611cf9580541dbc05272c0a
Young <hyper1003@gmail.com>
2022-10-05 19:34:15 +0900

    A77

--- sensor.py
+++ sensor.py
@@ -1,2 +1,3 @@
 print("[sensor.py] 인터페이스 정의에 의한 코드 작성 템플릿 or library or etc.......")
 print("[sensor.py] 센서 모듈 I/F .........300라인")
+print("[sensor.py] 최조 사용 후 버려졌으나 다시 필요하게된 모듈이 있네요...........300라인")
